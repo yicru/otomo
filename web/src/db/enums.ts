2a9aec7381d7282b545721d1fc862eb4942e8793
yuichiro miyamae <yu1ro.mya@gmail.com>
2024-01-08 17:37:12 +0900

    fix: build error

--- web/src/db/enums.ts
+++ web/src/db/enums.ts
@@ -1,7 +1,7 @@
 export const TaskStatus = {
-  COMPLETED: 'COMPLETED',
-  FAILED: 'FAILED',
-  IN_PROGRESS: 'IN_PROGRESS',
-  SCHEDULED: 'SCHEDULED',
-} as const
-export type TaskStatus = (typeof TaskStatus)[keyof typeof TaskStatus]
+    COMPLETED: "COMPLETED",
+    FAILED: "FAILED",
+    IN_PROGRESS: "IN_PROGRESS",
+    SCHEDULED: "SCHEDULED"
+} as const;
+export type TaskStatus = (typeof TaskStatus)[keyof typeof TaskStatus];
--- web/src/db/types.ts
+++ web/src/db/types.ts
@@ -1,44 +1,44 @@
-import type { ColumnType } from 'kysely'
+import type { ColumnType } from "kysely";
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>
-export type Timestamp = ColumnType<Date, Date | string, Date | string>
+  : ColumnType<T, T | undefined, T>;
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
-import type { TaskStatus } from './enums'
+import type { TaskStatus } from "./enums";
 
 export type Article = {
-  id: Generated<string>
-  user_id: string
-  url: string
-  title: string
-  og_image: string | null
-  content: string
-  created_at: Generated<Timestamp>
-  updated_at: Generated<Timestamp>
-}
+    id: Generated<string>;
+    user_id: string;
+    url: string;
+    title: string;
+    og_image: string | null;
+    content: string;
+    created_at: Generated<Timestamp>;
+    updated_at: Generated<Timestamp>;
+};
 export type SynthesisTask = {
-  id: Generated<string>
-  user_id: string
-  article_id: string
-  polly_task_id: string
-  engine: string | null
-  voice_id: string | null
-  status: TaskStatus | null
-  status_reason: string | null
-  request_characters: number | null
-  output_url: string | null
-  created_at: Generated<Timestamp>
-  updated_at: Generated<Timestamp>
-}
+    id: Generated<string>;
+    user_id: string;
+    article_id: string;
+    polly_task_id: string;
+    engine: string | null;
+    voice_id: string | null;
+    status: string | null;
+    status_reason: string | null;
+    request_characters: number | null;
+    output_url: string | null;
+    created_at: Generated<Timestamp>;
+    updated_at: Generated<Timestamp>;
+};
 export type User = {
-  id: Generated<string>
-  uid: string
-  email: string
-  created_at: Generated<Timestamp>
-  updated_at: Generated<Timestamp>
-}
+    id: Generated<string>;
+    uid: string;
+    email: string;
+    created_at: Generated<Timestamp>;
+    updated_at: Generated<Timestamp>;
+};
 export type DB = {
-  articles: Article
-  synthesis_tasks: SynthesisTask
-  users: User
-}
+    articles: Article;
+    synthesis_tasks: SynthesisTask;
+    users: User;
+};
